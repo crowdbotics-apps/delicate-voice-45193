4ef8825138bcf0b91c145c9a87cf3641cd602a43
Crowdbotics <team@crowdbotics.com>
2023-12-26 13:56:57 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "delicate-voice-45193",
+  url: "https://delicate-voice-45193.botics.co",
+  api: "https://delicate-voice-45193.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
